9ff6228a9abb2484547396cdcee230096ce76b73
NiceCupOfJAVA <32717143+NiceCupOfJAVA@users.noreply.github.com>
2017-10-11 14:44:02 -0500

    Updated Cage.cpp
    Added a bunch of comments

--- Cage.cpp
+++ Cage.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int main()
 {
-    int age;
-    int year;
-    int num;
+    int age; //Current age
+    int year; //Current year
+    int num; //Number of years advanced or reversed
     
     cout << "How old are you? ";
-    cin >> age;
+    cin >> age; //User puts in current age
     cout << "What is the year? ";
-    cin >> year;
+    cin >> year; //User puts in current year
     cout << "How far would you like to advance time? ";
-    cin >> num;
+    cin >> num; //User puts in number of years that will be advanced, can use a negative (-) to reverse time too
    { 
-    if (num > 0)
+    if (num > 0) // If time is advanced
         cout << "Cool! You'll be " << age+num << " in the year " << year+num<< "!" << endl;
-    else if (num == -age)
+    else if (num == -age) //If time reversed is equal to current age
         cout << "You where born in " << year-age << "!" << endl;
-    else if (num <0)
+    else if (num <0) // And If time is reversed
         cout << "You where " << age+num << " years from being born in " << year+num<< "!" << endl;
     else 
-       cout << "Hmmm. Somethings wrong here.";
+       cout << "Hmmm. Somethings wrong here."; //Just in case I screw something up, thats what else statments are for right?
    }
-    cout << "(" << num << " years.)";
+    cout << "(" << num << " years.)"; //Outputs the Number of Years Advanced or Devanced
     return 0;
 }
